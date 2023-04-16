751e74d89e92979f73bf35a18a03507f43d6c176
Robert S <robert4832@pm.me>
2023-04-16 17:01:49 +0200

    Completed code

--- README.md
+++ README.md
@@ -2,13 +2,22 @@
 This is a simple bash script that uses OpenSSL to generate a CA ceritifcate along with one or more corresponding server certificates. Very handy if you wanna create valid HTTPS-connections on your LAN.
 
 ## Usage
+Command:
 ```./generate.sh [DOMAIN] [CM_NAME] [PASS] [VALID_FOR_DAYS]```
 
-Example:<br>
+`VALID_FOR_DAYS` is set to 10 years (3650 days) by default.
+
+Example usage:<br>
 ```./generate.sh "*.your-domain.local" "YourCM" "your-super-secret-password" 397```
 
-The files will be placed in "output/[CM_NAME]", in this case "output/YourCM".
+The files will be placed in `output/[CM_NAME]`, in this case `output/YourCM`.
 
-You can then use "[DOMAIN].crt" and "[DOMAIN].key" in your webserver-config to establish valid HTTPS-connection.
+You can then use `[DOMAIN].crt` and "[DOMAIN].key" in your webserver-config to establish valid HTTPS-connection. In this case the filenames would be `*.your-domain.local.crt` and `*.your-domain.local.key`.
+
+Be sure to store the password if you want to add additional server certificates in the future.
+
+The generate additional server certificates just run the same command again and the 
 
 Note: It's recommended to run `history -c` afterwards since the shell history will reveal your CA password.
+
+Enjoy!
--- generate.sh
+++ generate.sh
@@ -1,26 +1,58 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
 DOMAIN=$1
 CA_NAME=$2
 PASS=$3
 DAYS=${4:-3650}
 OUTPUT_FOLDER="output/$CA_NAME/"
+CERT_OUTPUT_FOLDER="output/$CA_NAME/certificates/"
+
+red () {
+    tput setaf 1
+    echo $1
+    tput init
+}
+
+green () {
+    tput setaf 2
+    echo $1
+    tput init
+}
+
+cleanup() {
+    rm -f "${OUTPUT_FOLDER}v3.ext" "${OUTPUT_FOLDER}ca.srl" "${CERT_OUTPUT_FOLDER}${DOMAIN}.csr"
+}
 
 mkdir -p "$OUTPUT_FOLDER"
+mkdir -p "$CERT_OUTPUT_FOLDER"
 
-if [[ ! -f "ca.pem" ]]; then
-    if [[ ! -f "ca.key" ]]; then
+if [[ ! -f "${OUTPUT_FOLDER}ca.pem" ]]; then
+    if [[ ! -f "${OUTPUT_FOLDER}ca.key" ]]; then
         openssl genrsa -aes256 -out "${OUTPUT_FOLDER}ca.key" 4096 -passout "pass:$PASS"
     fi
     openssl req -x509 -new -nodes -key "${OUTPUT_FOLDER}ca.key" -days "$DAYS" -passin "pass:$PASS" -out "${OUTPUT_FOLDER}ca.pem" -subj "/CN=$CA_NAME"
 fi
 
-openssl req -nodes -newkey rsa:2048 -keyout "${OUTPUT_FOLDER}${DOMAIN}.key" -out "${OUTPUT_FOLDER}${DOMAIN}.csr" -subj "/CN=$DOMAIN"
+if [[ -f "${OUTPUT_FOLDER}certificates/${DOMAIN}.key" ]] || [[ -f "${CERT_OUTPUT_FOLDER}${DOMAIN}.crt" ]]; then
+    echo "Certificate for domain \"${DOMAIN}\" already exists."
+    exit 1;
+fi
+
+openssl req -nodes -newkey rsa:2048 -keyout "${CERT_OUTPUT_FOLDER}${DOMAIN}.key" -out "${CERT_OUTPUT_FOLDER}${DOMAIN}.csr" -subj "/CN=$DOMAIN"
 
 echo "# v3.ext
 [EXT]
-subjectAltName=DNS.0:*.ichiva.local" > "${OUTPUT_FOLDER}v3.ext"
+subjectAltName=DNS.0:$DOMAIN" > "${OUTPUT_FOLDER}v3.ext"
 
-openssl x509 -req -in "${OUTPUT_FOLDER}${DOMAIN}.csr" -CA "${OUTPUT_FOLDER}ca.pem" -CAkey "${OUTPUT_FOLDER}ca.key" -CAcreateserial -passin "pass:$PASS" -out "${OUTPUT_FOLDER}${DOMAIN}.crt" -days "$DAYS" -extensions EXT -extfile "${OUTPUT_FOLDER}v3.ext"
+if ! output=$(openssl x509 -req -in "${CERT_OUTPUT_FOLDER}${DOMAIN}.csr" -CA "${OUTPUT_FOLDER}ca.pem" -CAkey "${OUTPUT_FOLDER}ca.key" -CAcreateserial -passin "pass:$PASS" -out "${CERT_OUTPUT_FOLDER}${DOMAIN}.crt" -days "$DAYS" -extensions EXT -extfile "${OUTPUT_FOLDER}v3.ext"); then
+    echo $output
+    red "Could not generate certificate for domain \"${DOMAIN}\"."
+    rm -f "${CERT_OUTPUT_FOLDER}${DOMAIN}.key"
+    rm -f "${CERT_OUTPUT_FOLDER}${DOMAIN}.crt"
+    cleanup
+    exit 1;
+fi
 
-#rm -f "${OUTPUT_FOLDER}v3.ext" "${OUTPUT_FOLDER}ca.srl" "${OUTPUT_FOLDER}${DOMAIN}.csr"
+green "Cleaning up files"
+cleanup
+green "Successfully generated certificate for domain \"${DOMAIN}\"."
